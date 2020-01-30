3f8ddfa3dd7ec5d91fa3de52efa13675d89fa3ce
EC2 Default User <ec2-user@ip-172-31-46-119.ap-northeast-1.compute.internal>
2020-01-30 08:18:14 +0000

    header

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,3 @@ products_csv = CSV.readlines("db/products.csv")
 products_csv.each do |row|
   Product.create(title: row[1], image_url: row[2], address: row[5], review: row[6], image: row[7])
 end
-
-reviews_csv = CSV.readlines("db/reviews.csv")
-reviews_csv.each do |row|
-  Review.create(review: row[1], rate: row[2], product_id: row[3], user_id: row[6])
-end
